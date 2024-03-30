2cfb521e8082856fdcaa8fc39b4d6d82a99c063f
rem0930 <06araki@gmail.com>
2024-03-30 20:14:28 +0900

    make_mypage

--- app/controllers/api/mypages_controller.rb
+++ app/controllers/api/mypages_controller.rb
@@ -1,16 +1,27 @@
 class Api::MypagesController < ApplicationController
+    # before_action :post_params
+
     def show
         @user = current_user
+        @posts = current_user.posts
+        puts @profile_data
         if @user
             @profile_data = {
                 user_id: @user.id,
                 user_name: @user.name,
                 user_email: @user.email,
-                icon: @user.icon
+                icon: @user.icon,
+                posts: @posts.map { |post| post.as_json.merge(created_at: post.created_at.strftime('%B %d, %Y')) }, status: :ok
             }
             render json: @profile_data
         else
             render json: { error: "You are not logged in" }, status: :not_found
         end
     end
+
+        # private
+
+        # def post_params
+        #     params.require(:post).permit(:title, :image)
+        # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,7 @@ User.create!(
     name: 'Jonathan Joestar',
     email: 'jonathan@joestar.com',
     password: 'password',
-<<<<<<< HEAD
     icon: 'https://jojo-portal.com/special/digital-contents/assets/images/common/icon_golden-wind.png'
-=======
-    icon: 'icons/01.png'
->>>>>>> 3c064c74dbd7bccfce4c1b57c1c6a418048f72ad
 )
 
 User.create!(
